498ab0f4bc9a83b46a1ca6eb075e7274459d66fb
Diego Mota <diego.motasocial@gmail.com>
2024-12-15 22:17:54 -0300

    a

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
@@ -21,28 +20,4 @@ function carregar() {
         img.src = 'img/noite.png'
         document.body.style.background = '#384d54'
     }
-=======
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date() // PEGA HORA DO COMPUTADOR
-    var hora = data.getHours() // CAPTA HORA DA DATA
-    
-
-    msg.innerHTML = `Agora são ${hora} horas` // ESCREVE NO LUGAR DA ID
-
-    if (hora >= 0 && hora < 12) {
-        // BOM DIA
-        img.src = 'img/manha.png'
-        document.body.style.background = '#fab772'
-    } else if (hora >= 12 && hora < 18) {
-        //BOA TARDE
-        img.src = 'img/tarde.png'
-        document.body.style.background = '#785541'
-    } else {
-        //BOA NOITE
-        img.src = 'img/noite.png'
-        document.body.style.background = '#384d54'
-    }
->>>>>>> 4ab2d2ae3769c46a0640e599290de68c1b301720
 }
